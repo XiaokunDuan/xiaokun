7e24351bc397adaa1767c76a28854edf51b26b35
Xiaokun Duan <ylu665485@gmail.com>
2026-03-26 14:49:52 +0800

    feat: initial release of xiaokun npm package

---
+++ README.md
@@ -0,0 +1,18 @@
+# xiaokun
+
+```
+npm install xiaokun
+```
+
+```js
+const me = require('xiaokun');
+
+console.log(me.name.en);     // Theo Duan
+console.log(me.name.zh);     // 段晓坤
+console.log(me.company);     // ByteDance
+console.log(me.github.uri);  // https://github.com/XiaokunDuan
+```
+
+---
+
+21 · ByteDance · Beijing · [xiaokunduan.github.io](https://xiaokunduan.github.io)
---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: { zh: '段晓坤', en: 'Theo Duan' },
+  gender: 'male',
+  birthday: new Date('2004/03/27'),
+  age: new Date().getFullYear() - 2004,
+  location: { home: 'Beijing', country: 'Mainland China' },
+  email: 'i@xiaokun.me',
+  github: { id: 'XiaokunDuan', uri: 'https://github.com/XiaokunDuan' },
+  x: { id: 'TechMGR', uri: 'https://x.com/TechMGR' },
+  homepage: 'https://xiaokunduan.github.io',
+  blog: 'https://paragraph.xyz/@xiaokun',
+  company: 'ByteDance',
+};
